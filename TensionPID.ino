5c1701de535f66a531d71e82b5079c06ed1e5c83
Suremane25 <38380701+Suremane25@users.noreply.github.com>
2018-04-14 19:53:34 -0400

    Update TensionPID.ino
    its all differnt

--- TensionPID.ino
+++ TensionPID.ino
@@ -12,6 +12,7 @@
     v1.00           drop test, hardware initialization, encoder reading in interrupt, stepper functionality
     v1.01           quadrature encoding without missing counts. New encoder.
     v1.02           Streamlined velocity calculation. 1D Serial Communication. 1D 1 CubeSat Deployment
+    v1.03           Everything changed
     Next...           PID Tuning, 1D deployment with 2 CubeSat, ROS communication, 2D Deployment
     
 Vernier pin _____color________arduino pin
@@ -23,67 +24,51 @@ Vernier pin _____color________arduino pin
     Vpin6-----> white -------->Analog 1
 */
 
-#include <VernierLib.h>
-#include <TimerThree.h>
-#include <PID_v1.h>
 #include <Stepper.h>
+#include <PID_v1.h>
 
-VernierLib Vernier;
+double tension, output, setpoint, frictionForce;
+int angle = 0;
 
-volatile double tension, output, setpoint, counter;
-volatile double currentTime = 0, lastTime = 0;
-#define outputA 2
-#define outputB 3
-#define INTERRUPT 0
-#define INTERRUPT 1
-bool aState, bState;
-
-Stepper posstepper(200, 6, 7, 8, 9);
-Stepper negstepper(200, 9, 8, 7, 6);
-PID frictionDevice(&tension, &output, &setpoint, 1, 0, 1, DIRECT);
+Stepper lessT(200, 6, 7, 8, 9);
+Stepper moreT(200, 9, 8, 7, 6);
+PID frictionPID(&tension, &output, &setpoint, .12, 0, 0, DIRECT);
 
 void setup() {
+  setpoint = 10;
+  frictionPID.SetMode(AUTOMATIC);
+  frictionPID.SetOutputLimits(-10, 10);
+  Serial.begin(9600);
+  Serial1.begin(9600);
 
-  Timer3.initialize();
-  Timer3.attachInterrupt(control, 10000);                           //Timer1 overflows to trigger the interrupt every 0.01s
-  pinMode (outputA, INPUT);
-  pinMode (outputB, INPUT);
-    
-  Vernier.autoID();
-    
-  frictionDevice.SetMode(AUTOMATIC);
-  frictionDevice.SetSampleTime(100);
-  frictionDevice.SetOutputLimits(-100, 100); //~270 maximum
-
-  setpoint = 100;
-  Serial.begin (9600);
-  while (!Serial) {//Wait for USB serial to connect
-  }
 }
 
 void loop() {
-   
-  Serial.println(tension);
-    
-  //  Serial.print("\t");
-  //  Serial.print(output);
-  //  Serial.print("\t");
-  //  Serial.println(setpoint);
 
-  tension = Vernier.readSensor();
-    
-  if (output > 0) {                 // evaluate stepping for smooth output
-    posstepper.setSpeed(150);
-    posstepper.step(output);
+  if (Serial1.available()) {
+    tension = Serial1.read();
+    tension = (double) tension;
+    tension = map(tension, 0, 255, 0, 115.56);
   }
-  else if (output < 0) {
-    negstepper.setSpeed(150);
-    negstepper.step(-output);
+
+  frictionPID.Compute();
+  Serial.print(tension);
+  Serial.print("\t");
+  Serial.print(angle);
+  Serial.print("\t");
+  Serial.println(output);
+
+
+  if (output >= 0) {
+    moreT.setSpeed(100);
+    moreT.step(output);
   }
+
+  if (output < 0) {
+    lessT.setSpeed(100);
+    lessT.step(-output);
+  }
+
 }
 
-void control() {//Triggered by timer overflow. Must refresh slowly enough to completely run or will "lock" the rest of the program, never returning to the main loop.
-  
-  frictionDevice.Compute();
-}
-
+//frictionForce=.00000004*pow(angle, 3)- .00004*pow(angle, 2)+ 0.014*angle-0.5275;
