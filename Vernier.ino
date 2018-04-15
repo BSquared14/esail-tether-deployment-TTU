6909ca285bf4ea7c265ef687a93df924e0996991
BSquared14 <38379923+BSquared14@users.noreply.github.com>
2018-04-15 17:08:29 -0500

    Update Vernier.ino

--- Vernier.ino
+++ Vernier.ino
@@ -1,20 +1,20 @@
+
 /* VernierLibTutorialAnalogRead (v2017)
    This sketch reads a data point from a Vernier Analog (BTA)
    sensor once every half second and prints the sensor reading
    with units to the Serial Monitor.
-
    Plug the sensor into the Analog 1 port on the Vernier Arduino
    Interface Shield or into an Analog Protoboard Adapter wired
    to Arduino pin A0.
 */
 
 #include "VernierLib.h" //include Vernier functions in this sketch
-VernierLib Vernier; //create an instance of the VernierLib library
+#include <SoftwareSerial.h>
 
 float tension;//create global variable to store sensor reading
+int tensionInt;
+VernierLib Vernier; //create an instance of the VernierLib library
 
-
-#include <SoftwareSerial.h>
 // XBee's DOUT (TX) is connected to pin 2 (Arduino's Software RX)
 // XBee's DIN (RX) is connected to pin 3 (Arduino's Software TX)
 SoftwareSerial XBee(2, 3); // RX, TX
@@ -26,13 +26,34 @@ void setup() {
 }
 
 void loop() {
-  tension = Vernier.readSensor(); //read one data value
-  tension = (tension - .72)*1000;
-  tension=map(tension, 0,11530, 0, 255);
-  delay(100);
-  XBee.write(tension);
-  Serial.println(tension);
 
+  tension = Vernier.readSensor(); //read one data value
+  tension = (tension - .72);
+  tensionInt = (int) tension;
+  XBee.write(tensionInt);
+  Serial.print(tensionInt);
+  Serial.print("\t");
+  Serial.println(tension);
+  
+//  byte * b = (byte *) &tension;
+
+//  XBee.write(b[0]);
+//  XBee.write(b[1]);
+//  XBee.write(b[2]);
+//  XBee.write(b[3]);
+//  //XBee.write(".");
+//  XBee.write('\n');
+//
+//  Serial.print(b[3], HEX);
+//  Serial.print(b[2], HEX);
+//  Serial.print(b[1], HEX);
+//  Serial.print(b[0], HEX);
+//
+//  Serial.print("\t");
+//  Serial.println(tension, HEX);
+
+
+  delay(100);
 }
 
 //Vernier pin _____color________arduino pin
@@ -42,5 +63,3 @@ void loop() {
 //Vpin4----------> purple ----->Analog 5
 //Vpin5----------> grey ------->VCC
 //Vpin6----------> white ------>Analog 0
-
-
